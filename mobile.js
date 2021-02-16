06df34595e3dc55a636163a6f10b8471f91601e1
tomoki <tomoki171923@gmail.com>
2021-02-17 06:43:12 +0900

    add isTouchDevice function

--- mobile.js
+++ mobile.js
@@ -4,7 +4,19 @@
  * function :whether user device is mobile or not
  * @return {boolean} true or false
  */
-export function isMobile () {
+export function isMobile() {
   const regexp = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
   return window.navigator.userAgent.search(regexp) !== -1
+}
+
+/**
+ * function :whether user device is touch device or not
+ * @return {boolean} true or false
+ */
+export function isTouchDevice() {
+  if (window.ontouchstart === null) {
+    return false
+  } else {
+    return true
+  }
 }
